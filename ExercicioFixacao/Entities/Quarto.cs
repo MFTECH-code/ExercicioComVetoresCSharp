c15dfd4bdae78c73c9dbdcd07702fe7f27544698
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-01-28 20:41:18 -0300

    Criação das entidades principais

---
+++ ExercicioFixacao/Entities/Quarto.cs
@@ -0,0 +1,24 @@
+﻿namespace ExercicioFixacao.Entities
+{
+    public class Quarto
+    {
+        public Residente Residente { get; set; }
+        public int Numero { get; set; }
+
+        public Quarto()
+        {
+            
+        }
+        public Quarto(Residente residente, int numero)
+        {
+            Residente = residente;
+            Numero = numero;
+        }
+
+        public override string ToString()
+        {
+            return Residente
+                + "Quarto: " + Numero;
+        }
+    }
+}
---
+++ ExercicioFixacao/Entities/Residente.cs
@@ -0,0 +1,26 @@
+﻿namespace ExercicioFixacao.Entities
+{
+    public class Residente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Residente()
+        {
+            
+        }
+        public Residente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Nome
+                + "Email: "
+                + Email;
+        }
+    }
+}
